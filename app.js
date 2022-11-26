0899c2feef132786a7a769cd12a58f5dfad03f30
Denis <den-apple@ya.ru>
2022-11-26 11:20:40 +0200

    feat: написать контроллеры get для users

--- app.js
+++ app.js
@@ -9,18 +9,24 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/users', router);
 
+// mongoose.connect('mongodb://127.0.0.1:27017/mestodb', () => {
+//   console.log('Подключено к MongoDB!');
+
+//   app.listen(PORT, () => {
+//     console.log(`Приложение запущено на порту ${PORT}!`);
+//   });
+// });
+
 mongoose.connect(
   'mongodb://127.0.0.1:27017/mestodb',
   {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   },
   () => {
-    console.log('Connected to MongoDB!');
+    console.log('Подключено к MongoDB!');
 
     app.listen(PORT, () => {
-      console.log(`App listening on port ${PORT}!`);
+      console.log(`Приложение запущено на порту ${PORT}!`);
     });
   }
 );
--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,37 @@
-const getUsers = (req, res) => {};
-const getUser = (req, res) => {};
-const createUser = (req, res) => {};
+const User = require('../models/user');
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    return res.status(200).send(users);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send({ message: 'Произошла ошибка' });
+  }
+};
+
+const getUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).send({ message: 'Пользователь не найден' });
+    }
+
+    return res.status(200).send(user);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send({ message: 'Произошла ошибка' });
+  }
+};
+
+const createUser = (req, res) => {
+  // const { name, about, avatar } = req.body;
+  // console.log(name, about, avatar);
+  // User.create({ name, about, avatar })
+  //   .then((user) => res.send({ data: user }))
+  //   .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+};
 
 module.exports = { getUsers, getUser, createUser };

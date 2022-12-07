3076569c086f548884bef76b23750cf1acd16c1b
Denis <den-apple@ya.ru>
2022-12-07 20:51:05 +0200

    fix Добавление пользователя с некорректным url-адресом в поле avatar

--- validation/auth.js
+++ validation/auth.js
@@ -11,7 +11,7 @@ const createUserCelebrate = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().dataUri(),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),

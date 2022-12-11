d9287e27bff85a4a616dde3a401f2c784b15a6a8
Denis <den-apple@ya.ru>
2022-12-11 10:49:23 +0200

    chore: изменить число запросов limiter

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 450,
   message: 'Превышен лимит количества запросов к серверу',
 });
 
